a11b8b21753f73174b0883a7492215f8996eb892
JJ <joshua.james918@gmail.com>
2025-12-25 19:47:59 -0600

    blocked out routed that were not coded

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,11 @@ import express from 'express';
 import autosRoute from './routes/auto.route';
 import maintenanceRoute from './routes/maintenance.routes'
 import partsRoute from './routes/parts.route';
-import vehicleHistoryRoute from './routes/vehiclehistory.route';
-import usersRoute from './routes/users.route';
-import techniciansRoute from './routes/technicians.route';
-import shopRoute from './routes/shop.route';
-import serviceLogRoute from './routes/servicelog.route';
+// import vehicleHistoryRoute from './routes/vehiclehistory.route';
+// import usersRoute from './routes/users.route';
+// import techniciansRoute from './routes/technicians.route';
+// import shopRoute from './routes/shop.route';
+// import serviceLogRoute from './routes/serviceLog.route';
 import healthRoute from './routes/health.route';
 
 export * from "./types/api";
@@ -21,11 +21,11 @@ app.use(express.json());
 app.use('/api/autos', autosRoute);
 app.use('/api/maintenance', maintenanceRoute);
 app.use('/api/parts', partsRoute);
-app.use('/api/vehicle-history', vehicleHistoryRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/technicians', techniciansRoute);
-app.use('/api/shops', shopRoute);
-app.use('/api/service-logs', serviceLogRoute);
+// app.use('/api/vehicle-history', vehicleHistoryRoute);
+// app.use('/api/users', usersRoute);
+// app.use('/api/technicians', techniciansRoute);
+// app.use('/api/shops', shopRoute);
+// app.use('/api/service-logs', serviceLogRoute);
 app.use('/api/health', healthRoute);
 
 const PORT = process.env.PORT || 3000;
