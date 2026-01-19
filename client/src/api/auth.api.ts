ada2461e4ab44a182ea2a4e5ec105afb9aecd995
JJ <joshua.james918@gmail.com>
2026-01-18 19:12:52 -0600

    added auth.api to api on client side

---
+++ client/src/api/auth.api.ts
@@ -0,0 +1,23 @@
+import api from "./client";
+import type { ApiResponse } from "../types";
+
+type AuthUser = {
+  user_id: number;
+  email: string;
+};
+
+export async function login(email: string, password: string) {
+  const res = await api.post<ApiResponse<{ user: AuthUser; token: string }>>(
+    "/auth/login",
+    { email, password }
+  );
+  return res.data;
+}
+
+export async function register(email: string, password: string) {
+  const res = await api.post<ApiResponse<{ user: AuthUser; token: string }>>(
+    "/auth/register",
+    { email, password }
+  );
+  return res.data;
+}
